0c4b380d974651ab4ba39ddb8cd1599234f38bb1
jerricklc@gmail.com <jerricklc@gmail.com>
2022-10-18 17:13:26 -0400

    mvp

--- views/pokedex/Edit.jsx
+++ views/pokedex/Edit.jsx
@@ -1,9 +1,6 @@
 const React = require('react')
 const Default = require('../layouts/Default.jsx')
 
-
-
-
 class Edit extends React.Component {
 	render(){
         const {Name, Type, Image, Weakness, readyToFight, _id} = this.props.pokemon
@@ -12,7 +9,7 @@ class Edit extends React.Component {
 				<form method="POST" action={`/pokedex/${_id}?_method=PUT`}>
 					Name: <input type="text" name="Name" defaultValue={Name}></input><br/>
 					Type: <input type="text" name="Type" defaultValue={Type}></input><br/>
-					Type: <select name="type" id="typesOf"><option value="" selected="selected">Select Type</option></select><br/>
+					{/* Type: <select name="type" id="typesOf"><option value="" selected="selected">Select Type</option></select><br/> */}
 					Image: <input type="string" name="Image" defaultValue={Image}></input><br/>
 					Weakness: <input type="text" name="Weakness" defaultValue={Weakness}></input><br/>
 					Ready to Fight <input type="checkbox" name="readyToFight" defaultValue={readyToFight}></input><br/>
--- views/pokedex/New.jsx
+++ views/pokedex/New.jsx
@@ -5,14 +5,6 @@ const pokemonTypes = {
 	types: ["Normal", "Fire", "Water", "Grass", "Electric", "Ice", "Fighting", "Poison", "Ground", "Flying", "Psychic", "Bug", "Rock", "Ghost", "Dark", "Dragon", "Steel", "Fairy"]
 }
 
-// function selecting () {
-// 	const selectType = document.getElementById("typesOf")
-// 	for (let x in pokemonTypes){
-// 		selectType.options[selectType.options.length] = new Option(x,x);
-// 	}
-// }
-
-// selecting(pokemonTypes);
 
 class New extends React.Component {
 	render(){
@@ -21,7 +13,7 @@ class New extends React.Component {
 				<form method="POST" action="/pokedex">
 					Name: <input type="text" name="Name" placeholder="Pokemon Name"></input><br/>
 					Type: <input type="text" name="Type" placeholder="What is it's type"></input><br/>
-					Type: <select name="type" id="typesOf"><option value="" selected="selected">Select Type</option></select><br/>
+					{/* Type: <select name="type" id="typesOf"><option value="" selected="selected">Select Type</option></select><br/> */}
 					Image: <input type="string" name="Image" placeholder="Upload a image"></input><br/>
 					Weakness: <input type="text" name="Weakness" placeholder="What is it's weakness"></input><br/>
 					Ready to Fight <input type="checkbox" name="readyToFight"></input><br/>
@@ -31,5 +23,14 @@ class New extends React.Component {
 		)
 	}
 }
+
+// function selecting () {
+// 	const selectType = New.getElementById("typesOf")
+// 	for (let x in pokemonTypes){
+// 		selectType.options[selectType.options.length] = new Option(x,x);
+// 	}
+// }
+
+// selecting(pokemonTypes);
 
 module.exports = New
